a9a83c994a566441f8a5b76e6b71b645a555b8c6
uwuknight <trananhkhoile17@gmail.com>
2023-03-13 11:29:17 +0700

    Update my code

---
+++ 2. My code/StudyWinform/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClickThis = new Button();
+            lblHelloWorld = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // btnClickThis
+            // 
+            btnClickThis.Location = new Point(51, 455);
+            btnClickThis.Name = "btnClickThis";
+            btnClickThis.Size = new Size(75, 23);
+            btnClickThis.TabIndex = 0;
+            btnClickThis.Text = "Click this";
+            btnClickThis.UseVisualStyleBackColor = true;
+            btnClickThis.Click += btnClickThis_Click;
+            // 
+            // lblHelloWorld
+            // 
+            lblHelloWorld.AutoSize = true;
+            lblHelloWorld.Location = new Point(51, 499);
+            lblHelloWorld.Name = "lblHelloWorld";
+            lblHelloWorld.Size = new Size(38, 15);
+            lblHelloWorld.TabIndex = 1;
+            lblHelloWorld.Text = "label1";
+            lblHelloWorld.Click += lblHelloWorld_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(321, 170);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1068, 576);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(lblHelloWorld);
+            Controls.Add(btnClickThis);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Picture Viewer";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnClickThis;
+        private Label lblHelloWorld;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}

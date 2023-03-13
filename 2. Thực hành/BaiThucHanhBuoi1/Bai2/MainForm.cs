ea1c6e60d50bd7839845df162e066c0d708fc0d6
Lê Trần Anh Khôi <trananhkhoile17@gmail.com>
2023-03-13 14:24:11 +0700

    Update thuc hanh

---
+++ 2. Thực hành/BaiThucHanhBuoi1/Bai2/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void rdNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdNam.Checked)
+            {
+                MessageBox.Show("Bạn chọn giới tính Nam", "Thông báo");
+            }
+        }
+
+        private void rdNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdNu.Checked)
+            {
+                MessageBox.Show("Bạn chọn giới tính Nữ", "Thông báo");
+            }
+        }
+
+        private void btnToMau_Click(object sender, EventArgs e)
+        {
+            if(rdDo.Checked)
+            {
+                txtHopMau.BackColor = Color.Red;
+            }
+            else
+            {
+                txtHopMau.BackColor = Color.Green;
+            }
+        }
+    }
+}
---
+++ 2. Thực hành/BaiThucHanhBuoi1/BaiThucHanhBuoi1/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BaiThucHanhBuoi1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaoChep = new System.Windows.Forms.Button();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtSaoChep = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập tên của bạn:";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(346, 99);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 31);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "Xử lý";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bạn đã nhập:";
+            // 
+            // btnSaoChep
+            // 
+            this.btnSaoChep.Location = new System.Drawing.Point(465, 99);
+            this.btnSaoChep.Name = "btnSaoChep";
+            this.btnSaoChep.Size = new System.Drawing.Size(121, 31);
+            this.btnSaoChep.TabIndex = 4;
+            this.btnSaoChep.Text = "Sao chép";
+            this.btnSaoChep.UseVisualStyleBackColor = true;
+            this.btnSaoChep.Click += new System.EventHandler(this.btnSaoChep_Click);
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(335, 51);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(251, 22);
+            this.txtTen.TabIndex = 5;
+            this.txtTen.TextChanged += new System.EventHandler(this.txtTen_TextChanged);
+            // 
+            // txtSaoChep
+            // 
+            this.txtSaoChep.Location = new System.Drawing.Point(335, 160);
+            this.txtSaoChep.Name = "txtSaoChep";
+            this.txtSaoChep.Size = new System.Drawing.Size(251, 22);
+            this.txtSaoChep.TabIndex = 6;
+            this.txtSaoChep.TextChanged += new System.EventHandler(this.txtSaoChep_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.txtSaoChep);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.btnSaoChep);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Chương trình đầu tiên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSaoChep;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtSaoChep;
+    }
+}
+
